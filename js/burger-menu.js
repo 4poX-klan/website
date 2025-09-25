ffec516e0e4438e57f61c5f3f1daa3db06706f1b
Herald Klitcher <herald.klitcher@gmail.com>
2025-09-25 16:15:54 +0200

    menu

---
+++ js/burger-menu.js
@@ -0,0 +1,6 @@
+function burgerMenu() {
+    const burgerMenuOpen = document.getElementById('burger-menu-open')
+    const burgerMenuClose = document.getElementById('burger-menu-close')
+    burgerMenuOpen.classList.toggle('off')
+    burgerMenuClose.classList.toggle('on')
+}
