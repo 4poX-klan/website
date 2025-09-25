c290643ae428c999fe3827259738502f8663b968
Herald Klitcher <herald.klitcher@gmail.com>
2025-09-25 19:19:50 +0200

    1st-look

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,6 +1,10 @@
 function burgerMenu() {
     const burgerMenuOpen = document.getElementById('burger-menu-open')
     const burgerMenuClose = document.getElementById('burger-menu-close')
+    const burgerMenu = document.getElementById('burger-menu')
+    const bugerMenuOverlay = document.getElementById('burger-menu-overlay')
     burgerMenuOpen.classList.toggle('off')
     burgerMenuClose.classList.toggle('on')
+    burgerMenu.classList.toggle('on')
+    bugerMenuOverlay.classList.toggle('on')
 }
